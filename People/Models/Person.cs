f573473379ce0181bd85fa4313a1065b565eb8c1
JohanKYT <kevin.maquis@udla.edu.ec>
2025-07-01 08:12:12 -0500

    Se completo el modelo Person

--- People/Models/Person.cs
+++ People/Models/Person.cs
@@ -1,7 +1,10 @@
 ﻿using SQLite;
 namespace People.Models;
+[Table("people")]
 public class Person
 {
+    [PrimaryKey, AutoIncrement]
     public int Id { get; set; }
+    [PrimaryKey, AutoIncrement]
     public string Name { get; set; }
 }
